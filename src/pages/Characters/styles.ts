a71bb455a79709a25eec4ac97a61516adfbece2b
Luiz Silva <luiz.gsilva42@gmail.com>
2021-02-10 21:05:00 -0300

    update card

--- src/pages/Characters/styles.ts
+++ src/pages/Characters/styles.ts
@@ -1,13 +1,13 @@
 import styled, { css } from 'styled-components';
 
-interface ThumbnailDTO{
+interface ThumbnailDTO {
   thumbnail: {
     path: string;
     extension: string;
-  }
+  };
 }
 
-interface InputLabelProps{
+interface InputLabelProps {
   isFocused: boolean;
   isFilled: boolean;
 }
@@ -20,28 +20,26 @@ export const InputLabel = styled.div<InputLabelProps>`
   margin: 0 auto;
   width: 100%;
 
-
   label {
     display: flex;
     align-items: center;
     justify-content: center;
     height: 50px;
-    box-shadow:2px 2px 10px 1px rgba(0,0,0,0.3);
+    box-shadow: 2px 2px 10px 1px rgba(0, 0, 0, 0.3);
     padding: 0 10px;
     border-radius: 10px;
 
-    ${(props) => props.isFocused && css`
-      border: 1px solid;
-      background: #ed1d24;
-      `
-}
+    ${(props) => props.isFocused
+      && css`
+        border: 1px solid;
+        background: #ed1d24;
+      `}
 
     input {
       border: none;
-      background:transparent;
+      background: transparent;
       margin: 10px;
     }
-
   }
 
   svg#enter {
@@ -53,7 +51,6 @@ export const InputLabel = styled.div<InputLabelProps>`
     background: #f1f1f1;
     cursor: pointer;
   }
-
 `;
 
 export const Container = styled.div`
@@ -69,31 +66,35 @@ export const Card = styled.div`
   background: #f1f1f1;
   height: 450px;
   width: 300px;
-  margin: 10px;
+  margin: 30px;
   border-radius: 5px;
   overflow: hidden;
-  box-shadow:2px 2px 10px 1px rgba(0,0,0,0.3);
+  box-shadow: 2px 2px 10px 1px rgba(0, 0, 0, 0.3);
+  transition: all 1s;
 
-  h2, p {
+  h2,
+  p {
     padding: 5px;
     text-align: justify;
   }
 
   div#img {
-  height: 400px;
-  width: 100%;
-  background: url(${urlImg}) no-repeat center;
-  background-size: cover;
-  transition: height 1s;
+    height: 400px;
+    width: 100%;
+    background: url(${urlImg}) no-repeat center;
+    background-size: cover;
+    transition: height 1s;
   }
 
   &:hover {
-    div#img{
-    height: 100px;
+    div#img {
+      height: 100px;
     }
+
+    transform: scale(1.2, 1.2);
+    -webkit-box-shadow:0px 0px 50px 8px rgb(0,0,0);
+    box-shadow: 0px 0px 50px 8px rgb(0,0,0);
   }
-
-
 `;
 
 export const Img = styled.div`
@@ -110,14 +111,13 @@ export const ButtonMore = styled.div`
   align-items: center;
   justify-content: space-around;
   cursor: pointer;
-  box-shadow:2px 2px 10px 1px rgba(0,0,0,0.3);
+  box-shadow: 2px 2px 10px 1px rgba(0, 0, 0, 0.3);
   margin: 20px 100px;
   padding: 0 50px;
   border-radius: 5px;
   transition: background 0.3s;
 
-
-  &:hover{
+  &:hover {
     background: #ec1d24;
   }
 `;
